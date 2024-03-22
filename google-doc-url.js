eaa93e269eb46076a4a621898d473f399e584a78
Jack Dougherty <jack.dougherty@trincoll.edu>
2024-03-22 10:32:33 -0400

    archived CSVs

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,4 +1,5 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1UdKC0E2FN9VuzJ2p6vLzLtsCu0ZAiLpPVURJLrt3zFw/edit#gid=0'
+var googleDocURL = null;
+// storymap data is now archived in GitHub repo CSV folder, migrated away from old Google Sheet
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
